17228d71e04480a9a4bee4cb7441ffd9bdfa6d07
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2024-08-01 14:48:01 +0200

    Fix instantiation for ModelAndCollectionMessageModel

--- src/Model/ModelAndCollectionMessageModel.php
+++ src/Model/ModelAndCollectionMessageModel.php
@@ -4,15 +4,45 @@ namespace StanislavPivovartsev\InterestingStatistics\Common\Model;
 
 use StanislavPivovartsev\InterestingStatistics\Common\Contract\ModelInCollectionInterface;
 use StanislavPivovartsev\InterestingStatistics\Common\Contract\ProcessDataInterface;
+use StanislavPivovartsev\InterestingStatistics\Common\Exception\MoveAndCollectionMessageException;
 
 class ModelAndCollectionMessageModel extends AbstractMessageModel implements ProcessDataInterface
 {
     use StringJsonEncodableTrait;
 
+    private static array $map = [
+        'games' => GameMessageModel::class,
+        'eav_attribute' => EavAttributeModel::class,
+        'eav_attribute_int' => EavAttributeIntModel::class,
+        'eav_attribute_varchar' => EavAttributeVarcharModel::class,
+        'moves' => MoveSavableMessageModel::class,
+        'move_scores' => MoveScoreModel::class,
+    ];
+
     public function __construct(
         private readonly string $collection,
         private readonly ModelInCollectionInterface $model,
     ) {
+    }
+
+    public static function getInstance(...$data): static
+    {
+        $collectionClassInstance = self::getClassInstance($data['collection']);
+        $data['model'] = $collectionClassInstance::getInstance(...$data['model']);
+
+        return parent::getInstance(...$data);
+    }
+
+    /**
+     * @return \StanislavPivovartsev\InterestingStatistics\Common\Model\AbstractMessageModel|class-string
+     */
+    private static function getClassInstance(string $collection): string
+    {
+        if (!isset(self::$map[$collection])) {
+            throw new MoveAndCollectionMessageException('Unknown collection ' . $collection);
+        }
+
+        return self::$map[$collection];
     }
 
     protected function getData(): array
