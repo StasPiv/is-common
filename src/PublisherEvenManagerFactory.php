184fac942e98331560ec1d5986f8c6bf0bf7bbec
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2024-08-15 10:47:41 +0200

    Add factory

---
+++ src/PublisherEvenManagerFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace StanislavPivovartsev\InterestingStatistics\Common;
+
+use StanislavPivovartsev\InterestingStatistics\Common\Contract\EventManagerFactoryInterface;
+use StanislavPivovartsev\InterestingStatistics\Common\Contract\EventManagerInterface;
+use StanislavPivovartsev\InterestingStatistics\Common\Contract\LoggingSubscriberFactoryInterface;
+use StanislavPivovartsev\InterestingStatistics\Common\Enum\PublisherEventTypeEnum;
+use StanislavPivovartsev\InterestingStatistics\Common\EventManager;
+
+class PublisherEvenManagerFactory implements EventManagerFactoryInterface
+{
+    public function __construct(
+        private readonly LoggingSubscriberFactoryInterface $loggingSubscriberFactory,
+    ) {
+    }
+
+    public function createEventManager(): EventManagerInterface
+    {
+        $eventManager = new EventManager();
+
+        foreach ($this->getLoggingSubscriberEvents() as $loggingSubscriberEvent) {
+            $eventManager->subscribe(
+                $loggingSubscriberEvent,
+                $this->loggingSubscriberFactory->createLoggingSubscriber($loggingSubscriberEvent),
+            );
+        }
+
+        return $eventManager;
+    }
+
+    /**
+     * @return array<\StanislavPivovartsev\InterestingStatistics\Common\Enum\EventTypeInterface>
+     */
+    protected function getLoggingSubscriberEvents(): array
+    {
+        return [
+            PublisherEventTypeEnum::MessageCountGreaterThanLimit,
+            PublisherEventTypeEnum::MessageCountLessThanLimit,
+        ];
+    }
+}
