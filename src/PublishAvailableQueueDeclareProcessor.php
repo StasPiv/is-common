2bdadae0a9305f5215b670459c051321cd01b53f
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2024-08-19 17:46:57 +0200

    Fix true/false condition for final batch

--- src/PublishAvailableQueueDeclareProcessor.php
+++ src/PublishAvailableQueueDeclareProcessor.php
@@ -2,12 +2,16 @@
 
 namespace StanislavPivovartsev\InterestingStatistics\Common;
 
+use StanislavPivovartsev\InterestingStatistics\Common\Contract\EventManagerInterface;
 use StanislavPivovartsev\InterestingStatistics\Common\Contract\QueueBatchConfigurationInterface;
+use StanislavPivovartsev\InterestingStatistics\Common\Enum\PublisherEventTypeEnum;
+use StanislavPivovartsev\InterestingStatistics\Common\Model\DataAwareProcessDataModel;
 
 class PublishAvailableQueueDeclareProcessor implements Contract\QueueDeclareProcessorInterface
 {
     public function __construct(
         private readonly QueueBatchConfigurationInterface $queueBatchConfiguration,
+        private readonly EventManagerInterface $eventManager,
     ) {
     }
 
@@ -15,7 +19,18 @@ class PublishAvailableQueueDeclareProcessor implements Contract\QueueDeclareProc
     {
         $queueSizeLimit = $this->queueBatchConfiguration->getQueueSizeLimit($queue);
         $batchSize = $this->queueBatchConfiguration->getBatchSize($queue);
+        $readyForPublish = $messageCount + $batchSize < $queueSizeLimit;
 
-        return $messageCount + $batchSize < $queueSizeLimit;
+        if (!$readyForPublish) {
+            $this->eventManager->notify(
+                PublisherEventTypeEnum::QueueOverloadedForFinalBatch,
+                new DataAwareProcessDataModel([
+                    'queueSizeLimit' => $queueSizeLimit,
+                    'batchSize' => $batchSize,
+                ])
+            );
+        }
+
+        return !$readyForPublish;
     }
 }
