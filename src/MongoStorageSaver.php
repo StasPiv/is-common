80b6589f9db2eee7e4cea7a3e602b59c71cbd21a
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2024-09-21 18:14:58 +0200

    Log exception when model save failed

--- src/MongoStorageSaver.php
+++ src/MongoStorageSaver.php
@@ -3,12 +3,16 @@
 namespace StanislavPivovartsev\InterestingStatistics\Common;
 
 use MongoDB\Database;
+use StanislavPivovartsev\InterestingStatistics\Common\Contract\EventManagerInterface;
 use StanislavPivovartsev\InterestingStatistics\Common\Contract\ModelInCollectionInterface;
+use StanislavPivovartsev\InterestingStatistics\Common\Enum\ProcessEventTypeEnum;
+use StanislavPivovartsev\InterestingStatistics\Common\Model\DataAwareProcessDataModel;
 
 class MongoStorageSaver implements Contract\StorageSaverInterface
 {
     public function __construct(
         private readonly Database $database,
+        private readonly EventManagerInterface $eventManager,
     ) {
     }
 
@@ -16,15 +20,22 @@ class MongoStorageSaver implements Contract\StorageSaverInterface
     {
         $data = $model->getDataForSave();
 
-        if (!isset($data['_id'])) {
-            $data['_id'] = $model->getId();
-        }
-
         if (isset($data['id'])) {
+            $data['_id'] = $data['id'];
+
             unset($data['id']);
         }
 
-        $result = $this->database->selectCollection($collection)->insertOne($data);
+        try {
+            $result = $this->database->selectCollection($collection)->insertOne($data);
+        } catch (\Throwable $exception) {
+            $this->eventManager->notify(
+                ProcessEventTypeEnum::ModelSaveFailed,
+                new DataAwareProcessDataModel(['exception' => $exception->getMessage(),])
+            );
+
+            return false;
+        }
 
         return $result->getInsertedCount() > 0;
     }
