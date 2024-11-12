393ef428a8453094c07bcf004041c4e2bf6e9336
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2024-11-12 16:41:49 +0100

    Add useful model

---
+++ src/Model/BSONDocumentModel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace StanislavPivovartsev\InterestingStatistics\Common\Model;
+
+use MongoDB\Model\BSONDocument;
+use StanislavPivovartsev\InterestingStatistics\Common\Contract\ModelInCollectionInterface;
+
+class BSONDocumentModel implements ModelInCollectionInterface
+{
+    use StringJsonEncodableTrait;
+    
+    public function __construct(
+        private readonly BSONDocument $document,  
+    ) {
+    }
+
+    public function getId(): string
+    {
+        return $this->document->offsetGet('_id');
+    }
+
+    public function setId(string $id): void
+    {
+        $this->document->offsetSet('_id', $id);
+    }
+
+    public function hasId(): bool
+    {
+        return $this->document->offsetExists('_id');
+    }
+
+    public function getDataForSave(): array
+    {
+        return $this->document->getArrayCopy();
+    }
+
+    public function getDataForSerialize(): array
+    {
+        return $this->document->getArrayCopy();
+    }
+}
