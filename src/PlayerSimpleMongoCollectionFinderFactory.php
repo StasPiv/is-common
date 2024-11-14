77448dc5e3bc43edc92abc84cfe408fccfb6783f
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2024-11-14 10:18:51 +0100

    Add simple finder for players

---
+++ src/PlayerSimpleMongoCollectionFinderFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace StanislavPivovartsev\InterestingStatistics\Common;
+
+use StanislavPivovartsev\InterestingStatistics\Common\Contract\CollectionFinderInterface;
+use StanislavPivovartsev\InterestingStatistics\Common\Contract\MongoDatabaseFactoryInterface;
+
+class PlayerSimpleMongoCollectionFinderFactory implements Contract\CollectionFinderFactoryInterface
+{
+    public function __construct(
+        private readonly MongoDatabaseFactoryInterface $mongoDatabaseFactory,
+    ) {
+    }
+
+    public function createCollectionFinder(): CollectionFinderInterface
+    {
+        return new PlayerMongoCollectionFinder($this->mongoDatabaseFactory->createDatabase(), true);
+    }
+}
